c575a61d44f9c75c6d8e4341a3d4f742cb6a7ea0
naoyashiga <naoyashiga0@gmail.com>
2024-06-29 16:02:50 +0900

    feat: 固定のファイル名から文字列検索

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::env;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    for line in search(&config.query, &config.filename) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "rs";
+        let contents = "\
+src/main.rs
+poem.txt
+";
+
+        assert_eq!(vec!["src/main.rs"], search(query, contents));
+    }
+}
