8e17e4e943fd15ade7e5973ea50f9e3f31491a4e
naoyashiga <naoyashiga0@gmail.com>
2024-07-02 23:04:50 +0900

    feat: use stack

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut results = Vec::new();
     println!("All File Paths:====================");
     explore_dir(&config.query, &config.filepath, &mut results)?;
-    
+
     println!("Results:====================");
     for result in results {
         println!("{}", result);
@@ -31,28 +31,33 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn explore_dir(query:&str, dir: &str, results: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
+fn explore_dir(query: &str, dir: &str, results: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
     if !Path::new(dir).exists() {
         return Err(format!("{} does not exist", dir).into());
     }
-    let entries = fs::read_dir(dir)?;
-    for entry in entries {
-        let entry = entry?;
-        let file_type = entry.file_type()?;
-        let file_path = entry.file_name();
-        let full_path_dir = format!("{}/{}", dir, file_path.to_string_lossy());
 
-        if file_type.is_dir() {
-            // println!("Dir: {:?}", full_path_dir);
-            explore_dir(&query, &full_path_dir, results)?;
-        } else {
-            println!("File: {:?}", full_path_dir);
-            if full_path_dir.contains(query) {
-                results.push(full_path_dir);
+    let mut stack = vec![dir.to_string()];
+
+    while let Some(current_dir) = stack.pop() {
+        if !Path::new(&current_dir).exists() {
+            return Err(format!("{} does not exist", current_dir).into());
+        }
+
+        let entries = fs::read_dir(&current_dir)?;
+        for entry in entries {
+            let entry = entry?;
+            let file_type = entry.file_type()?;
+            let file_path = entry.file_name();
+            let full_path_dir = format!("{}/{}", current_dir, file_path.to_string_lossy());
+
+            if file_type.is_dir() {
+                stack.push(full_path_dir.clone());
+            } else {
+                println!("File: {:?}", full_path_dir);
+                if full_path_dir.contains(query) {
+                    results.push(full_path_dir);
+                }
             }
-            // if search(&query, &full_path_dir) {
-            //     results.push(full_path_dir);
-            // }
         }
     }
     Ok(())
